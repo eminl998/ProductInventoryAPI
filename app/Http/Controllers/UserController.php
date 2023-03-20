54be7183216223d701f49d4e7362ad5d2c72f628
Emin Lika <eminlika998@gmail.com>
2023-03-20 15:06:38 +0100

    20.03.2023

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class UserController extends Controller
 {
@@ -11,59 +12,75 @@ class UserController extends Controller
     {
         $this->middleware('auth:api');
     }
+
     public function index()
     {
         $users = User::all();
 
         return response()->json($users);
     }
+
     public function show($id)
     {
-        $user = User::find($id);
-
-        if (!$user) {
+        try {
+            $user = User::findOrFail($id);
+            return response()->json($user);
+        } catch (ModelNotFoundException $e) {
             return response()->json(['error' => 'User not found'], 404);
         }
-
-        return response()->json($user);
     }
+
     public function store(Request $request)
     {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'username' => 'required|string|max:255|unique:users',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
         $user = new User();
-        $user->name = $request->input('name');
-        $user->username = $request->input('username');
-        $user->email = $request->input('email');
-        $user->password = bcrypt($request->input('password'));
+        $user->name = $validatedData['name'];
+        $user->username = $validatedData['username'];
+        $user->email = $validatedData['email'];
+        $user->password = bcrypt($validatedData['password']);
         $user->save();
 
         return response()->json($user, 201);
     }
+
     public function update(Request $request, $id)
     {
-        $user = User::find($id);
+        try {
+            $user = User::findOrFail($id);
 
-        if (!$user) {
+            $validatedData = $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|email|max:255|unique:users,email,' . $user->id,
+                'password' => 'nullable|string|min:8',
+            ]);
+
+            $user->name = $validatedData['name'];
+            $user->email = $validatedData['email'];
+            if (isset($validatedData['password'])) {
+                $user->password = bcrypt($validatedData['password']);
+            }
+            $user->save();
+
+            return response()->json($user);
+        } catch (ModelNotFoundException $e) {
             return response()->json(['error' => 'User not found'], 404);
         }
-
-        $user->name = $request->input('name');
-        $user->email = $request->input('email');
-        $user->password = bcrypt($request->input('password'));
-        $user->save();
-
-        return response()->json($user);
     }
+
     public function destroy($id)
     {
-        $user = User::find($id);
-
-        if (!$user) {
+        try {
+            $user = User::findOrFail($id);
+            $user->delete();
+            return response()->json(['message' => 'User deleted']);
+        } catch (ModelNotFoundException $e) {
             return response()->json(['error' => 'User not found'], 404);
         }
-
-        $user->delete();
-
-        return response()->json(['message' => 'User deleted']);
     }
-
 }
--- routes/api.php
+++ routes/api.php
@@ -1,31 +1,52 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-// Register a new user
-Route::post('/register', [RegisterController::class, 'register']);
-
-// Login an existing user
-Route::post('/login', [LoginController::class, 'login']);
+use App\Http\Controllers\UserController;
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
     Route::post('register', 'register');
-    Route::post('logout', 'logout');
-    Route::post('refresh', 'refresh');
-
 });
+Route::post('/login', [LoginController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/users', [UserController::class, 'index'])->name('index');
+    Route::get('/users', [UserController::class, 'index'])->name('index');
+    Route::put('/users', [UserController::class, 'index'])->name('index');
+    Route::delete('/users', [UserController::class, 'index'])->name('index');
+});
+
+
+
+
+
+// User routes
+// Route::middleware('auth:api')->group(function () {
+//     Route::get('/users', 'UserController@index');
+//     Route::get('/users/{id}', 'UserController@show');
+//     Route::put('/users/{id}', 'UserController@update');
+//     Route::delete('/users/{id}', 'UserController@destroy');
+// });
+
+// Product routes
+// Route::middleware('auth:api')->group(function () {
+//     Route::get('/products', 'ProductController@index');
+//     Route::get('/products/{id}', 'ProductController@show');
+//     Route::post('/products', 'ProductController@store');
+//     Route::put('/products/{id}', 'ProductController@update');
+//     Route::delete('/products/{id}', 'ProductController@destroy');
+// });
+
+// Category routes
+// Route::middleware('auth:api')->group(function () {
+//     Route::get('/categories', 'CategoryController@index');
+//     Route::get('/categories/{id}', 'CategoryController@show');
+//     Route::post('/categories', 'CategoryController@store');
+//     Route::put('/categories/{id}', 'CategoryController@update');
+//     Route::delete('/categories/{id}', 'CategoryController@destroy');
+//     Route::get('/categories/{id}/products', 'CategoryController@products');
+// });
