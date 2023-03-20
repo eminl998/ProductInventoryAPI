b924904f2d5a60b15babf6b4c9bf236ea94c6eca
Emin Lika <eminlika998@gmail.com>
2023-03-20 15:22:20 +0100

    20.03.2023

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,8 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
     Route::post('register', 'register');
 });
-Route::post('/login', [LoginController::class, 'login']);
 
+// User routes
 Route::middleware('auth:api')->group(function () {
     Route::get('/users', [UserController::class, 'index'])->name('index');
     Route::get('/users', [UserController::class, 'index'])->name('index');
@@ -20,6 +20,24 @@ Route::middleware('auth:api')->group(function () {
     Route::delete('/users', [UserController::class, 'index'])->name('index');
 });
 
+// Product routes
+Route::middleware('auth:api')->group(function () {
+    Route::get('/products', 'ProductController@index');
+    Route::get('/products/{id}', 'ProductController@show');
+    Route::post('/products', 'ProductController@store');
+    Route::put('/products/{id}', 'ProductController@update');
+    Route::delete('/products/{id}', 'ProductController@destroy');
+});
+
+// Category routes
+Route::middleware('auth:api')->group(function () {
+    Route::get('/categories', 'CategoryController@index');
+    Route::get('/categories/{id}', 'CategoryController@show');
+    Route::post('/categories', 'CategoryController@store');
+    Route::put('/categories/{id}', 'CategoryController@update');
+    Route::delete('/categories/{id}', 'CategoryController@destroy');
+    Route::get('/categories/{id}/products', 'CategoryController@products');
+});
 
 
 
