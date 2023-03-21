1c9905e141ae5e896403ca10d6a5c73255adec73
Emin Lika <eminlika998@gmail.com>
2023-03-21 10:04:40 +0100

    21.03.2023

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\UserController;
 
 Route::controller(AuthController::class)->group(function () {
@@ -14,21 +11,20 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('register', 'register');
 });
 
-// User routes
-Route::middleware('auth:api')->group(function () {
-    Route::get('/users', [UserController::class, 'index'])->name('index');
-    Route::get('/users{id}', [UserController::class, 'show'])->name('show');
-    Route::put('/users', [UserController::class, 'update'])->name('update');
-    Route::delete('/users', [UserController::class, 'destroy'])->name('delete');
+//User routes
+Route::controller(UserController::class)->group(function () {
+    Route::get('/users', 'index');
+    Route::get('/users{id}', 'show');
+    Route::put('/users', 'update');
+    Route::delete('/users', 'delete');
 });
 
 // Product routes
-Route::middleware('auth:api')->group(function () {
-    Route::get('/products',[ProductController::class, 'index'])->name('index');
-    Route::get('/products/{id}', [ProductController::class, 'show'])->name('show');
-    Route::post('/products', [ProductController::class, 'store'])->name('store');
-    Route::put('/products/{id}', [ProductController::class, 'update'])->name('update');
-    Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('delete');
+Route::controller(ProductController::class)->group(function(){
+    Route::get('/product', 'index');
+    Route::get('/product{id}', 'show');
+    Route::put('/product', 'update');
+    Route::delete('/product', 'delete');
 });
 
 // Category routes
