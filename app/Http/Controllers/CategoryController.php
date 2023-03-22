767d9b97a717199312a557886ce05a696194d77a
Emin Lika <eminlika998@gmail.com>
2023-03-22 14:43:52 +0100

    22.03.2023

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,64 +4,41 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
-use Illuminate\Validation\Rule;
 
 class CategoryController extends Controller
 {
     public function index()
     {
         $categories = Category::all();
-
-        return response()->json([
-            'data' => $categories,
-        ]);
+        return response()->json($categories);
     }
 
     public function store(Request $request)
     {
-        $request->validate([
-            'name' => 'required|unique:categories,name',
-        ]);
-
-        $category = new Category();
-        $category->name = $request->input('name');
-        $category->save();
-
-        return response()->json([
-            'data' => $category,
-        ]);
+        $category = Category::create($request->all());
+        return response()->json($category);
     }
 
     public function show(Category $category)
     {
-        return response()->json([
-            'data' => $category,
-        ]);
+        return response()->json($category);
     }
 
     public function update(Request $request, Category $category)
     {
-        $request->validate([
-            'name' => [
-                'required',
-                Rule::unique('categories')->ignore($category->id),
-            ],
-        ]);
-
-        $category->name = $request->input('name');
-        $category->save();
-
-        return response()->json([
-            'data' => $category,
-        ]);
+        $category->update($request->all());
+        return response()->json($category);
     }
 
     public function destroy(Category $category)
     {
         $category->delete();
+        return response()->json(null, 204);
+    }
 
-        return response()->json([
-            'message' => 'Category deleted successfully',
-        ]);
+    public function products(Category $category)
+    {
+        $products = $category->products;
+        return response()->json($products);
     }
 }
